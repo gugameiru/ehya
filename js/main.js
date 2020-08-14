5378bd9e975e8448efb641be505357205c1ddeac
gugameiru <gugameiru@gmail.com>
2020-08-14 19:59:23 +0300

    fix: trending section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
