69927db064b6a504001db28ee0e4c06f4c8af50f
gugameiru <gugameiru@gmail.com>
2020-08-15 02:55:24 +0300

    create: stories section

--- js/main.js
+++ js/main.js
@@ -2,9 +2,22 @@ var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+  autoplay: {
+    delay: 7000,
+  },
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
   },
+});
+
+var swiperContainer = document.querySelector(".swiper-container");
+
+swiperContainer.addEventListener("mouseenter", () => {
+  mySwiper.autoplay.stop();
+});
+
+swiperContainer.addEventListener("mouseleave", () => {
+  mySwiper.autoplay.start();
 });
